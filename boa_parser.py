99a6edff542c79c55263603c35e4d081b3a4ab81
Brian Cottingham <spiffytech@gmail.com>
2010-07-12 12:56:16 -0400

    Script now searches for translations file. Also, added guidepost comments.

--- boa_parser.py
+++ boa_parser.py
@@ -4,44 +4,87 @@
 # 2010-07-08
 # Parses the Bank of America "downloadable text" files and spits out Ledger-formated data
 
+import time
 from optparse import OptionParser
+import os
 import re
+import yaml
+
+# Find the file with human-readable translations for BoA statement descriptions
+boa_translations_filename = "boa_translate.yaml"
+boa_translations_locs = (
+    os.path.join(os.path.dirname(__file__), boa_translations_filename),
+    os.path.join(os.path.expanduser("~"), "." + boa_translations_filename),
+    os.path.join(os.path.normpath("/etc/"), boa_translations_filename)
+)
+for loc in boa_translations_locs:
+    if os.path.exists(loc):
+        boa_translations = loc
+        break
+else: 
+    print "Can't find Bank of America description translation file. \nPlease put it in one of these locations:\n"
+    for loc in boa_translations_locs:
+        print loc
+    exit(1)
+
 
 def print_equity(boa_statement):
+    """Prints a transaction that initializes the account balance. Used when creating a new ledger file."""
     for line in open(boa_statement):
         if line.startswith("Beginning balance as of"):
             init_balance = line.split()[-1]
+            
             init_date = line.split()[-2]
-            init_balance = "$" + init_balance
+            time_tuple = time.strptime(init_date, "%m/%d/%Y")
+            init_date = time.strftime("%Y/%m/%d", time_tuple)
 
     print '''
 %(date)s * Opening balance
-    Assets:Bank:Checking  %(balance)s
+    Assets:Bank:Checking  $%(balance)s
     Equity:Opening Balance
 ''' % {"date": init_date, "balance": init_balance}
 
 
 def print_ledger(boa_statement, init=False):
-    regex = re.compile(r'''(?P<date>\d{2}/\d{2}/\d{4})
+    """Parses the BoA statement file and prints all transactions in a Ledger-friendly format."""
+
+    # Regex to match/parse transactions in the BoA statement file
+    regex = re.compile(r'''(?P<date>\d{2}/\d{2}/\d{4})  # Date of the transaction
                                 \s{2,}
-                                (?P<payee>.*)
+                                (?P<payee>.*)  # Transaction description(payee)
                                 \s{2,}
-                                (?P<amount>-*[0-9,.]+)
+                                (?P<amount>-*[0-9,.]+)  # Transaction value
                                 \s{2,}
-                                [0-9,.]+$
+                                [0-9,.]+$  # Running account balance
         ''', re.VERBOSE)
+
+    desc_patterns = list(yaml.load_all(open(boa_translations)))
 
     for line in open(boa_statement):
         line = line.strip()
         rematch = re.match(regex, line)
         if rematch:
             trans_date, payee, amount = rematch.groups()
+            amount = amount.replace(",", "")
 
-            credit = "something" if amount.startswith("-") else "Assets:Bank:Checking"
-            debit = "Assets:Bank:Checking" if amount.startswith("-") else "something"
-            amount = str(float(amount)*-1) if amount.startswith("-") else amount
+            # Convert date to Ledger-happy format
+            time_tuple = time.strptime(trans_date, "%m/%d/%Y")
+            trans_date = time.strftime("%Y/%m/%d", time_tuple)
 
-            status = "*" if init is True else ""
+            credit = "something"  # Dummy category to use when we don't know the real category
+            debit = "Assets:Bank:Checking"  # Assume all transactions are for a checking account
+            amount = str(float(amount)*-1)
+
+            # Parse the BoA descriptions for payee/category
+            for pattern in desc_patterns:
+                if re.search(pattern["pattern"], payee):
+                    try:
+                        credit = pattern["category"]
+                    except KeyError:
+                        pass
+                    payee = pattern["payee"]
+
+            status = "*" if init is True else ""  # If scrit is passed "-i", all transactions are cleared
 
             transaction = '''
 %(date)s %(status)s %(payee)s
